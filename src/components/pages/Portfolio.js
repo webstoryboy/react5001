19703183080f848189b57c706179010e7a48956c
webstoryboy <webstupids@gmail.com>
2021-10-21 18:32:05 +0900

    2021.10.21

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../layout/Header';
+import Contents from '../layout/Contents';
+import Footer from '../layout/Footer';
+
+function Portfolio(){
+    return (
+        <div>
+            <Header />
+            <Contents />
+            <Footer />
+        </div>
+    )
+}
+
+export default Portfolio;
